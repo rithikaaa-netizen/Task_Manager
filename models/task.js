11461869c3f85736b80be898d015a217b9fa1433
rithikaaa-netizen <rithikap164@gmail.com>
2025-08-02 14:16:56 +0530

    Initial changes

---
+++ models/task.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose');
+const TaskSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required:[true, 'Title is required']
+    },
+    description: {
+        type: String
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+},{ timestamps: true});
+module.exports=mongoose.model('Task', TaskSchema);
---
+++ route/tasks.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const Task = require('../models/task');
+const router=express.Router();
+router.get('/', async (req,res) => {
+    const tasks = await Task.find();
+    res.json(tasks);
+});
+router.get('/:id', async (req,res) => {
+    try{
+        const task= await Task.findById(req.params.id);
+        if(!task) res.status(404).json({error: 'Task not found'});
+        res.json(task);
+    }
+    catch(err){
+        res.status(400).json({error :'Incalid ID'});
+    }
+});
+router.post('/', async (req,res) => {
+    try{
+        const task = new Task(req.body);
+        await task.save();
+        res.status(201).json(task);
+    }
+    catch(err){
+        res.status(400).json({error : err.message});
+    }
+});
+router.delete('/:id', async (req,res) => {
+    try{
+        const task= await Task.findByIdAndDelete(req.params.id);
+        if(!task) 
+            return res.status(400).json({error : "Task not found"});
+        res.json({message : "Task deleted"});
+    }
+    catch (err){
+        res.status(400).json({error : "Invalid ID"});
+    }
+});
+module.exports = router;
